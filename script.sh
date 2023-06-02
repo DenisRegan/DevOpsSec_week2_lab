eab88252ba25888ff5e4d51797ed64087220d3b4
DenisRegan <denismartinoregan@hotmail.com>
2023-06-02 21:38:47 +0100

    step 9

--- script.sh
+++ script.sh
@@ -2,9 +2,13 @@
 #mkdir fol_1
 #mkdir fol_2
 
-touch fol_1/1_1.txt
-touch fol_1/1_2.txt
-touch fol_1/1_3.txt
-touch fol_2/1_1.txt
-touch fol_2/1_2.txt
-touch fol_2/1_3.txt
+#touch fol_1/1_1.txt
+#touch fol_1/1_2.txt
+#touch fol_1/1_3.txt
+#touch fol_2/1_1.txt
+#touch fol_2/1_2.txt
+#touch fol_2/1_3.txt
+
+#Step 8
+rm -r fol_1
+rm -r fol_2
